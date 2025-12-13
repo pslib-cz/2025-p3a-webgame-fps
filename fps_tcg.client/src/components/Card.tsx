15bafae2a52f3785d6573618728857dc54ed0b6b
PetrL45 <146720250+PetrL45@users.noreply.github.com>
2025-12-13 20:14:27 +0100

    save5(card-upravi-nehotove)

--- fps_tcg.client/src/components/Card.tsx
+++ fps_tcg.client/src/components/Card.tsx
@@ -1,13 +1,53 @@
 import { type CardType } from "../types";
-import { useState, type FC } from "react";
+import { type FC } from "react";
 
 type CardProps = {
-   
+    id: number,
+    name: string,
+    type: CardType,
+
+    health?: number,
+    shield?: number,
+    skill1Name?: string,
+    skill1Damage?: number,
+    skill1Cost?: { [key: string]: number },
+    skill2Name?: string,
+    skill2Effect?: string,
+    skill2Cost?: { [key: string]: number },
+
+    supportCost?: { [key: string]: number },
+    supportEffect?: string
 }
 
-const Card: FC<CardProps> = () =>{
+const Card: FC<CardProps> = (props) =>{
+    
+    if(props.type === 'character'){
+        return(
+            <div className="card character-card">
+                <p className="card-name">{props.name}</p>
+                <div className="card-stats">
+                    <p>{props.health}</p>
+                    <p>{props.shield}</p>
+                </div>
+            </div>
+        )
+    }
+    if(props.type === 'support'){
+
+        return(
+            <div className="card support-card">
+                <p className="cost-tag"></p>
+                <p className="card-name">{props.name}</p>
+                <p className="card-effect">{props.supportEffect}</p>
+            </div>
+        )
+    }
+
     return(
-        
+        <div className="card error-card">
+            <p>ERROR: Unknown Card Type!</p>
+            <p>Card ID: {props.id}</p>
+        </div>
     )
 }
 export default Card;
--- fps_tcg.client/src/types/index.ts
+++ fps_tcg.client/src/types/index.ts
@@ -1,2 +1,2 @@
-export type CardType = "character" | "attack" | "support";
+export type CardType = "character" | "support";
 export type GameView = 'Play' | 'Dice_Roll';
