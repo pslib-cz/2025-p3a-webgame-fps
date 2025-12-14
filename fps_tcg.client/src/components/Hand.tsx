82cef1dd89b4cbc8e1226c487f876ebc3e3e7d27
PetrL45 <146720250+PetrL45@users.noreply.github.com>
2025-12-14 18:23:25 +0100

    save10(card-select)

--- fps_tcg.client/src/components/Hand.tsx
+++ fps_tcg.client/src/components/Hand.tsx
@@ -1,22 +1,31 @@
 import Card, { type CardProps } from "./Card";
 import {type FC} from "react";
 import style from './Hand.module.css'
-import cat from "../assets/blehcat.png"
 
-type HandCard = Pick<CardProps, 'id' | 'name' | 'type' | 'supportCost' | 'supportEffect'>;
+type HandCard = Pick<CardProps, 'id' | 'name' | 'imgSrc' | 'type' | 'supportCost' | 'supportEffect'>;
 
 type HandProps = {
     cards: HandCard[];
-    onCardSelect: (cardId: number) => void;
-    selectedCardId: number | null;
+    onCharacterActive: (cardId: number) => void;
+    activeCharacterId: number | null;
 }
 
-const Hand: FC<HandProps> = () => {
+const Hand: FC<HandProps> = ({
+    cards,
+    activeCharacterId,
+    onCharacterActive
+    }) => {
     return(
         <div className={style.handContainer}>
-            <Card id={1} name={"bleh cat"} type={"character"} imgSrc={cat} health={10} shield={10}/>
-            <Card id={1} name={"bleh cat"} type={"character"} imgSrc={cat} health={10} shield={10}/>
-            <Card id={1} name={"bleh cat"} type={"character"} imgSrc={cat} health={10} shield={10}/>
+            {cards.map(card => (
+                <Card
+                    key={card.id}
+                    {...card}
+                    isSelected={card.id === activeCharacterId}
+                    onClick={() => onCharacterActive(card.id)}
+                />
+            ))}
+
         </div>
     );
 }
--- fps_tcg.client/src/pages/PlayPage.tsx
+++ fps_tcg.client/src/pages/PlayPage.tsx
@@ -3,12 +3,26 @@ import { type FC } from "react"
 import style from '../styles/PlayPage.module.css'
 import cardBack90 from '../assets/cardback-90.png'
 import Hand from "../components/Hand"
+import cat from "../assets/blehcat.png"
+import type { CardType } from '../types'
 
 type PlayPageProps = {
-    onCardPicked: () => void;
+    
 }
 
+const characters = [
+    { id: 1, name: "Bleh Cat", type: "character" as CardType, imgSrc: cat, health: 10, shield: 10 },
+    { id: 2, name: "Bleh Cat", type: "character" as CardType, imgSrc: cat, health: 10, shield: 10 },
+    { id: 3, name: "Bleh Cat", type: "character" as CardType, imgSrc: cat, health: 10, shield: 10 }
+];
+
 export const PlayPage: FC<PlayPageProps> = () => {
+    const [activeCard, setActiveCard] = useState<number | null>(null)
+
+    const handleCharacterSelect = (cardId: number) => {
+        setActiveCard(cardId)
+    }
+
     return(
         <div className={style.playPageBody}>
             <div className={style.deckBox}>
@@ -17,8 +31,8 @@ export const PlayPage: FC<PlayPageProps> = () => {
             </div>
             <button className={style.endRoundButton}>END ROUND</button>
             <div className={style.playPanel}>
-                <Hand cards={[]} onCardSelect={function (_cardId: number): void {} } selectedCardId={null} />
-                <Hand cards={[]} onCardSelect={function (_cardId: number): void {} } selectedCardId={null} />
+                <Hand cards={characters} activeCharacterId={activeCard} onCharacterActive={handleCharacterSelect}/>
+                <Hand cards={characters} activeCharacterId={activeCard} onCharacterActive={handleCharacterSelect} />
             </div>
             <div className={style.dicePanel}></div>
         </div>
