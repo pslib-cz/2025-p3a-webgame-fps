3809b3f3409b5b5c56ab0ad47b2f0863199badca
sebastian.rybar.023 <sebastian.rybar.023@PC2022-21>
2026-02-02 09:26:04 +0100

    ZMENA VELKOSTI PRVIHO PISMENA cmuk

--- FPS_TCG.Server/Models/Card.cs
+++ FPS_TCG.Server/Models/Card.cs
@@ -10,22 +10,22 @@ namespace FPS_TCG.Server.Models
 
         public required string Name { get; set; }
 
-        public required string type { get; set; }
+        public required string Type { get; set; }
 
-        public int health { get; set; }
-        public int shield { get; set; }
+        public int Health { get; set; }
+        public int Shield { get; set; }
 
         public required string Skill1Name { get; set; }
         public int Skill1Damage { get; set; }
         public required int Skill1Cost { get; set; }
 
         public required string Skill2Name { get; set; }
-        public required string skill2Effect { get; set; }
+        public required string Skill2Effect { get; set; }
         public required int Skill2Damage { get; set; }
         public required int Skill2Cost { get; set; }
 
-        public int supportCost { get; set; }
-        public required string supportEffect { get; set; }
+        public int SupportCost { get; set; }
+        public required string SupportEffect { get; set; }
 
 
         public ICollection<Deck> Decks { get; set; } = new List<Deck>();
--- FPS_TCG.Server/Models/DeckCreateDto.cs
+++
@@ -1,8 +0,0 @@
-//namespace FPS_TCG.Server.Models
-//{
-//    public class DeckCreateDto
-//    {
-//        public required string Name { get; set; }
-//        public required List<int> CardIds { get; set; } = new();
-//    }
-//}
