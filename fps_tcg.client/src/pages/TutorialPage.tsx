ba3199ac841e075bfffcf88b535bbccfb1a4fc7f
SebastianRybar <147390978+SebastianRybar@users.noreply.github.com>
2026-02-09 19:46:31 +0100

    Tutorial summary

--- fps_tcg.client/src/pages/TutorialPage.tsx
+++ fps_tcg.client/src/pages/TutorialPage.tsx
@@ -73,6 +73,21 @@ export const TutorialPage: FC = () =>{
                             <li><b>Healer-Heal</b>  A support skill that gives hp to one of the player cards (player can choose the target)</li>
                             <li><b>Rogue-Assasination</b>   Can be used on an opponent's card if it is alive (player can choose the target)</li>
                         </ul>
+                        <h3>Rules Summary</h3>
+                        <ol>
+                            <li><b>Deck Building:</b><p>Click button <strong>Edit Decks</strong> after that click the + sign. Click on one of those three top + signs and choose 3 Character Cards after that click on one of the bottom ones and choose 6 Support Cards. You can also change the name by clicking on the text above</p></li>
+                            <li><b>Saving deck:</b><p>Click the save button under.</p></li>
+                            <li><b>Active deck:</b><p>Click BACK on the top left corner. Click on the deck that you made and under EDIT button there is SET ACTIVE button that sets your deck as active.</p></li>
+                            <li><b>Starting a match:</b><p>Go back to the main page and click Start New Game. Click on one of your three cards to set it active. After you did that you will roll eight dices which you can reroll by clicking on them.</p></li>
+                            <li><b>Valid Moves:</b>
+                            <ul>
+                                <li>Click on your card to see available actions, after that, choose the amount of dices it costs and click on the ability you want to use. Skill2 can be performed only with specific symbol dice but skill1 can be used with any dice.</li>
+                                <li>Click on one of your non-active character cards, choose one dice, and switch your active card by clicking on the confirm button on the left bottom.</li>
+                                <li>Click on the deck of support cards on the bottom right, click on a symbol you want to exchange, click on the dice you want to exchange and click play.</li>
+                            </ul>
+                            </li>
+                            <li><b>Ending a turn:</b><p>After you have used all your actions or decided to end your turn, click the End Turn button.</p></li>
+                        </ol>
                 </div>
             </div>
         </div>
